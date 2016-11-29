e3d56d8b70c42a783a5b686873ed24bce643a1aa
ThomasvLingen <thovali96@gmail.com>
2016-11-29 18:28:58 +0100

    refactor: use OpenGL 2.x instead of 3.x
    This is done since my trusty old Thinkpad with it's IGX doesn't bloody
    support OpenGL 3.x

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "imgui.h"
-#include "imgui_impl_sdl_gl3.h"
-#include <GL/gl3w.h>
+#include "imgui_impl_sdl.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 using std::cout;
 using std::endl;
@@ -12,52 +12,43 @@ int main()
     std::cout << "Hi" << std::endl;
 
     // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
-        cout << "Error: " << SDL_GetError() << endl;
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        cout << "SDL init Error: " << SDL_GetError() << endl;
         return -1;
     }
 
-    // Setup window (whole lot of magic settings)
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    // Setup window (lots of magic numbers!)
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_DisplayMode current;
     SDL_GetCurrentDisplayMode(0, &current);
-    SDL_Window *window = SDL_CreateWindow("ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
-    std::cout << SDL_GetError() << std::endl;
+    SDL_Window *window = SDL_CreateWindow("ImGui SDL2+OpenGL example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_SHOWN);
     SDL_GLContext glcontext = SDL_GL_CreateContext(window);
-    std::cout << SDL_GetError() << std::endl;
-
-    if(gl3wInit()){
-        std::cout << "Failed gl3wInit" << std::endl;
-    }
-
 
     // Setup ImGui binding
-    ImGui_ImplSdlGL3_Init(window);
-    // Some color that IMGUI uses
-    ImVec4 clear_color = ImColor(114, 144, 154);
+    ImGui_ImplSdl_Init(window);
+
+    // Background colour
+    ImVec4 clear_color = (ImVec4)ImColor(114, 144, 154);
 
     bool running = true;
     while (running) {
-        // Handle SDL events
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            ImGui_ImplSdlGL3_ProcessEvent(&event);
+            ImGui_ImplSdl_ProcessEvent(&event);
             if (event.type == SDL_QUIT) {
                 running = false;
             }
         }
 
-        ImGui_ImplSdlGL3_NewFrame(window);
+        ImGui_ImplSdl_NewFrame(window);
 
-        // ImGui::Begin("FUCK");
-        ImGui::Text("HALLO FAGGOT");
-        // ImGui::End();
+        ImGui::Begin("DE SCHERM");
+        ImGui::Text("HALLO IMGUI");
+        ImGui::End();
 
         glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
