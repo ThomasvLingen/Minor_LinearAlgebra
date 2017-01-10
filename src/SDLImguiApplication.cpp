9225da1bcc5f065d3920198faeb86fe5658cbb64
ThomasvLingen <thovali96@gmail.com>
2017-01-10 13:19:35 +0100

    refactor: change coordinate system

--- src/SDLImguiApplication.cpp
+++ src/SDLImguiApplication.cpp
@@ -134,35 +134,35 @@ void SDLImguiApplication::run()
         glBegin(GL_QUADS);        // Draw The Cube Using quads
         glLoadIdentity();
         glColor3f(0.0f,1.0f,0.0f);    // Color Blue
-        glVertex3f( 0.5f, 0.5f,-0.5f);    // Top Right Of The Quad (Top)
-        glVertex3f(-0.5f, 0.5f,-0.5f);    // Top Left Of The Quad (Top)
-        glVertex3f(-0.5f, 0.5f, 0.5f);    // Bottom Left Of The Quad (Top)
-        glVertex3f( 0.5f, 0.5f, 0.5f);    // Bottom Right Of The Quad (Top)
+        glVertex3i( 50, 50,-50);    // Top Right Of The Quad (Top)
+        glVertex3i(-50, 50,-50);    // Top Left Of The Quad (Top)
+        glVertex3i(-50, 50, 50);    // Bottom Left Of The Quad (Top)
+        glVertex3i( 50, 50, 50);    // Bottom Right Of The Quad (Top)
         glColor3f(1.0f,0.5f,0.0f);    // Color Orange
-        glVertex3f( 0.5f,-0.5f, 0.5f);    // Top Right Of The Quad (Bottom)
-        glVertex3f(-0.5f,-0.5f, 0.5f);    // Top Left Of The Quad (Bottom)
-        glVertex3f(-0.5f,-0.5f,-0.5f);    // Bottom Left Of The Quad (Bottom)
-        glVertex3f( 0.5f,-0.5f,-0.5f);    // Bottom Right Of The Quad (Bottom)
+        glVertex3i( 50,-50, 50);    // Top Right Of The Quad (Bottom)
+        glVertex3i(-50,-50, 50);    // Top Left Of The Quad (Bottom)
+        glVertex3i(-50,-50,-50);    // Bottom Left Of The Quad (Bottom)
+        glVertex3i( 50,-50,-50);    // Bottom Right Of The Quad (Bottom)
         glColor3f(1.0f,0.0f,0.0f);    // Color Red
-        glVertex3f( 0.5f, 0.5f, 0.5f);    // Top Right Of The Quad (Front)
-        glVertex3f(-0.5f, 0.5f, 0.5f);    // Top Left Of The Quad (Front)
-        glVertex3f(-0.5f,-0.5f, 0.5f);    // Bottom Left Of The Quad (Front)
-        glVertex3f( 0.5f,-0.5f, 0.5f);    // Bottom Right Of The Quad (Front)
+        glVertex3i( 50, 50, 50);    // Top Right Of The Quad (Front)
+        glVertex3i(-50, 50, 50);    // Top Left Of The Quad (Front)
+        glVertex3i(-50,-50, 50);    // Bottom Left Of The Quad (Front)
+        glVertex3i( 50,-50, 50);    // Bottom Right Of The Quad (Front)
         glColor3f(1.0f,1.0f,0.0f);    // Color Yellow
-        glVertex3f( 0.5f,-0.5f,-0.5f);    // Top Right Of The Quad (Back)
-        glVertex3f(-0.5f,-0.5f,-0.5f);    // Top Left Of The Quad (Back)
-        glVertex3f(-0.5f, 0.5f,-0.5f);    // Bottom Left Of The Quad (Back)
-        glVertex3f( 0.5f, 0.5f,-0.5f);    // Bottom Right Of The Quad (Back)
+        glVertex3i( 50,-50,-50);    // Top Right Of The Quad (Back)
+        glVertex3i(-50,-50,-50);    // Top Left Of The Quad (Back)
+        glVertex3i(-50, 50,-50);    // Bottom Left Of The Quad (Back)
+        glVertex3i( 50, 50,-50);    // Bottom Right Of The Quad (Back)
         glColor3f(0.0f,0.0f,1.0f);    // Color Blue
-        glVertex3f(-0.5f, 0.5f, 0.5f);    // Top Right Of The Quad (Left)
-        glVertex3f(-0.5f, 0.5f,-0.5f);    // Top Left Of The Quad (Left)
-        glVertex3f(-0.5f,-0.5f,-0.5f);    // Bottom Left Of The Quad (Left)
-        glVertex3f(-0.5f,-0.5f, 0.5f);    // Bottom Right Of The Quad (Left)
+        glVertex3i(-50, 50, 50);    // Top Right Of The Quad (Left)
+        glVertex3i(-50, 50,-50);    // Top Left Of The Quad (Left)
+        glVertex3i(-50,-50,-50);    // Bottom Left Of The Quad (Left)
+        glVertex3i(-50,-50, 50);    // Bottom Right Of The Quad (Left)
         glColor3f(1.0f,0.0f,1.0f);    // Color Violet
-        glVertex3f( 0.5f, 0.5f,-0.5f);    // Top Right Of The Quad (Right)
-        glVertex3f( 0.5f, 0.5f, 0.5f);    // Top Left Of The Quad (Right)
-        glVertex3f( 0.5f,-0.5f, 0.5f);    // Bottom Left Of The Quad (Right)
-        glVertex3f( 0.5f,-0.5f,-0.5f);    // Bottom Right Of The Quad (Right)
+        glVertex3i( 50, 50,-50);    // Top Right Of The Quad (Right)
+        glVertex3i( 50, 50, 50);    // Top Left Of The Quad (Right)
+        glVertex3i( 50,-50, 50);    // Bottom Left Of The Quad (Right)
+        glVertex3i( 50,-50,-50);    // Bottom Right Of The Quad (Right)
         glEnd();            // End Drawing The Cube
         glRotatef(1,1.0,0.0,0.0);
         glRotatef(2,0.0,1.0,0.0);
@@ -189,9 +189,9 @@ void SDLImguiApplication::_set_OpenGL_coordinate_mode()
     // Setup OpenGL for coordinate system drawing
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    /*glOrtho(-this->_screen_width/2, this->_screen_width/2,
+    glOrtho(-this->_screen_width/2, this->_screen_width/2,
             -this->_screen_height/2, this->_screen_height/2,
-            -1, 1);*/
+            -100, 100);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -213,8 +213,6 @@ void SDLImguiApplication::_clear_screen()
     // glClear(GL_COLOR_BUFFER_BIT);
     // Clear the screen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
 }
 
 void SDLImguiApplication::_GUI_logic()
