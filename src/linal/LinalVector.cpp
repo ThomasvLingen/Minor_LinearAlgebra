e5f96767172e009174db0eda8afe2cb827aaee9e
ThomasvLingen <thovali96@gmail.com>
2017-01-11 18:56:21 +0100

    feat: add LinalVectors
    These include the inproduct and crossproduct methods

---
+++ src/linal/LinalVector.cpp
@@ -0,0 +1,44 @@
+//
+// Created by mafn on 1/11/17.
+//
+
+#include "LinalVector.hpp"
+
+LinalVector::LinalVector(double x, double y, double z)
+: LinalMatrix({
+    {x},
+    {y},
+    {z}
+})
+{
+
+}
+
+LinalVector::LinalVector(const LinalMatrix& other)
+: LinalMatrix(other.values)
+{
+
+}
+
+double LinalVector::in_product(LinalVector& a, LinalVector& b)
+{
+    return a[x] * b[x] +
+           a[y] * b[y] +
+           a[z] * b[z];
+}
+
+LinalVector LinalVector::cross_product(LinalVector& a, LinalVector& b)
+{
+    return LinalVector {
+        a[y] * b[z] - b[y] * a[z],
+        b[x] * a[z] - a[x] * b[z],
+        a[x] * b[y] - b[x] * a[y]
+    };
+}
+
+double LinalVector::operator[](size_t index)
+{
+    return this->values[index][0];
+}
+
+
---
+++ src/linal/LinalVector.hpp
@@ -0,0 +1,28 @@
+//
+// Created by mafn on 1/11/17.
+//
+
+#ifndef LINEARALGEBRA_LINALVECTOR_HPP
+#define LINEARALGEBRA_LINALVECTOR_HPP
+
+
+#include "LinalMatrix.hpp"
+
+class LinalVector : public LinalMatrix<double> {
+public:
+    LinalVector(double x, double y, double z);
+    LinalVector(const LinalMatrix& other);
+
+    double operator[](size_t index);
+
+    static double in_product(LinalVector& a, LinalVector& b);
+    static LinalVector cross_product(LinalVector& a, LinalVector& b);
+private:
+    static const size_t x = 0;
+    static const size_t y = 1;
+    static const size_t z = 2;
+
+};
+
+
+#endif //LINEARALGEBRA_LINALVECTOR_HPP
